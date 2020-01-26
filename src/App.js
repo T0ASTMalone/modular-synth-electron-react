63e249851104ce2a28d41ceefdafd21cda61c039
Miguel Ponce <miguelaponce258@gmail.com>
2020-01-26 14:04:35 -0700

    feat: implement mock rack with test modules

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import github from "./assets/images/github.png";
+import { defaultTemplate } from "./app-menu";
+import Rack from "./components/Rack/Rack";
 
 const TitleBar = window.require("frameless-titlebar");
 
-const electron = window.require("electron").remote;
+// const electron = window.require("electron").remote;
 
-const menu = electron.Menu.getApplicationMenu();
+// const menu = electron.Menu.getApplicationMenu();
 
 function App() {
   return (
-    <div className="App">
-      <TitleBar icon={github} app="Electron" menu={menu} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <TitleBar icon={github} app='Electron' menu={defaultTemplate} />
+      <Rack />
     </div>
   );
 }
