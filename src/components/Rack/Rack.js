bab3ab35d53d4a427f782ef15d3bd6eb09a3721e
Miguel Ponce <miguelaponce258@gmail.com>
2020-02-06 20:25:56 -0700

    feat: implement dynamic imports for rack modules

--- src/components/Rack/Rack.js
+++ src/components/Rack/Rack.js
@@ -1,14 +1,66 @@
-import React from "react";
-import Oscillator from "../modules/Oscillator/Oscillator.js";
-import Filter from "../modules/Filter/FIlter";
+import React, { useEffect, useState } from "react";
 import "./Rack.css";
 
 const Rack = () => {
+  const [loadedModules, loadModules] = useState([]);
+  const [currentModules, setCurrentModules] = useState([]);
+
+  useEffect(() => {
+    const currentModules = [
+      "Oscillator",
+      "Filter",
+      "Filter",
+      "Oscillator",
+      "Filter"
+    ];
+    // reduce array to only contain one of each of the current modules
+    const imports = currentModules.filter(
+      (item, i) => currentModules.indexOf(item) === i
+    );
+
+    const importModules = mods =>
+      // map over reduced array instead, in order to import only one of each module
+      Promise.all(mods.map(mod => import(`../modules/${mod}/${mod}.js`)));
+
+    importModules(imports).then(loadedModules => {
+      loadModules(loadedModules);
+      // set currentModules in state to use for rendering once modules have been imported
+      setCurrentModules(currentModules);
+    });
+  }, []);
+
+  // useState to hold current modules array
+  // map over current modules in state instead of loaded modules
+
   return (
     <div className="rack">
-      <Oscillator />
-      <Oscillator />
-      <Filter />
+      <div className="rack__controls">
+        {/* rack controls */}
+        <button>Stop</button>
+        <button>Play</button>
+        <button>Rec</button>
+      </div>
+
+      <div className="rack__modules">
+        {currentModules ? (
+          currentModules.map((name, i) => {
+            const loadedMod = loadedModules.find(
+              mod => mod.default.name === name
+            );
+            const Module = loadedMod.default;
+            return <Module key={i} />;
+          })
+        ) : (
+          <></>
+        )}
+      </div>
+
+      <div className="rack__visualAudio">
+        {/* 
+          General information about the output 
+          i.e. visualization of the output, spectrum analyzer, (kind of like op-1 stuff)
+        */}
+      </div>
     </div>
   );
 };
--- src/context/MsContext.js
+++ src/context/MsContext.js
@@ -2,15 +2,9 @@ import React, { Component } from "react";
 
 const MsContext = React.createContext({
   error: null,
-<<<<<<< HEAD
-  sidebar: false,
-  patch: [],
-  sbView: "modules",
-=======
   sidebar: null,
   sbContent: "",
   loaded: [],
->>>>>>> 005144d7f6bc58f3cbf1d827741c5e93bfe9b5cf
   clearContext: () => {},
   setSbContent: () => {},
   toggleSidebar: () => {}
@@ -24,21 +18,6 @@ export class MsProvider extends Component {
     this.state = {
       error: null,
       sidebar: false,
-<<<<<<< HEAD
-      sbView: "modules"
-    };
-  }
-
-  setSbView = label => {
-    console.log(label, ": context label");
-    let sbView = label;
-    this.setState({ sbView });
-  };
-
-  toggleSidebar = () => {
-    let sidebar = this.state.sidebar;
-    this.setState({ sidebar: !sidebar });
-=======
       sbContent: "",
       loaded: []
     };
@@ -67,7 +46,6 @@ export class MsProvider extends Component {
     this.setState({
       sidebar: !sidebar
     });
->>>>>>> 005144d7f6bc58f3cbf1d827741c5e93bfe9b5cf
   };
 
   clearContext = () => {
@@ -78,16 +56,11 @@ export class MsProvider extends Component {
     const value = {
       error: this.state.error,
       sidebar: this.state.sidebar,
-<<<<<<< HEAD
-      sbView: this.state.sbView,
-      setSbView: this.setSbView,
-=======
       sbContent: this.state.sbContent,
       loaded: this.state.loaded,
 
       load: this.load,
       setSbContent: this.setSbContent,
->>>>>>> 005144d7f6bc58f3cbf1d827741c5e93bfe9b5cf
       toggleSidebar: this.toggleSidebar,
       clearContext: this.clearContext
     };
