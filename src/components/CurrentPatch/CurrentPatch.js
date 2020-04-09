73c1a14a7a5ecdd1987935e2502b43ab8e787fed
T0ASTMalone <miguelaponce258@gmail.com>
2020-04-09 09:22:34 -0700

    feat: implement current patch in sidebar

--- src/components/CurrentPatch/CurrentPatch.js
+++ src/components/CurrentPatch/CurrentPatch.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./CurrentPatch.css";
+import MsContext from "../../context/MsContext";
 
 const CurrentPatch = () => {
+  const context = useContext(MsContext);
+
+  const { nodes, unload } = context;
+
+  const renderListItem = (type, id) => {
+    return (
+      <div className="list-item" key={id}>
+        <span>{type}</span>
+        {type !== "main-gain" ? (
+          <button onClick={() => unload(id)}>X</button>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  };
+
   return (
     <div className="curretn-patch">
       <h2>Current Patch</h2>
+      {Object.keys(nodes).map((mod) => renderListItem(nodes[mod].type, mod))}
     </div>
   );
 };
